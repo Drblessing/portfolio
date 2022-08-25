ee15ad08b058d5dd2925d80de2245e84f58331bc
Danny Blessing <52829488+Drblessing@users.noreply.github.com>
2022-08-25 14:41:27 -0700

    move it above

--- src/components/Header.js
+++ src/components/Header.js
@@ -26,7 +26,7 @@ class Header extends Component {
 
   render() {
     if (this.props.sharedData) {
-      var name = this.props.sharedData.name;
+      var name = this.props.sharedData.name.replace(' | Portfolio', '');
       this.titles = this.props.sharedData.titles
         .map((x) => [x.toUpperCase(), 1500])
         .flat();
@@ -46,12 +46,52 @@ class Header extends Component {
         id='home'
         style={{ height: window.innerHeight - 140, display: 'block' }}
       >
+        <Switch
+          checked={this.state.checked}
+          onChange={this.onThemeSwitchChange}
+          offColor='#baaa80'
+          onColor='#353535'
+          className='react-switch mx-auto'
+          width={90}
+          height={40}
+          uncheckedIcon={
+            <span
+              className='iconify'
+              // data-icon="twemoji:owl"
+              data-inline='false'
+              style={{
+                display: 'block',
+                height: '100%',
+                fontSize: 25,
+                textAlign: 'end',
+                marginLeft: '20px',
+                color: '#353239',
+              }}
+            ></span>
+          }
+          checkedIcon={
+            <span
+              className='iconify'
+              // data-icon='noto-v1:sun-with-face'
+              data-inline='false'
+              style={{
+                display: 'block',
+                height: '100%',
+                fontSize: 25,
+                textAlign: 'end',
+                marginLeft: '10px',
+                color: '#353239',
+              }}
+            ></span>
+          }
+          id='icon-switch'
+        />
         <div className='row aligner' style={{ height: '100%' }}>
           <div className='col-md-12'>
             <div>
               <span
                 className='iconify header-icon'
-                data-icon='la:laptop-code'
+                data-icon='la:laptop'
                 data-inline='false'
               ></span>
               <br />
@@ -61,46 +101,6 @@ class Header extends Component {
               <div className='title-container'>
                 <HeaderTitleTypeAnimation />
               </div>
-              <Switch
-                checked={this.state.checked}
-                onChange={this.onThemeSwitchChange}
-                offColor='#baaa80'
-                onColor='#353535'
-                className='react-switch mx-auto'
-                width={90}
-                height={40}
-                uncheckedIcon={
-                  <span
-                    className='iconify'
-                    // data-icon="twemoji:owl"
-                    data-inline='false'
-                    style={{
-                      display: 'block',
-                      height: '100%',
-                      fontSize: 25,
-                      textAlign: 'end',
-                      marginLeft: '20px',
-                      color: '#353239',
-                    }}
-                  ></span>
-                }
-                checkedIcon={
-                  <span
-                    className='iconify'
-                    // data-icon='noto-v1:sun-with-face'
-                    data-inline='false'
-                    style={{
-                      display: 'block',
-                      height: '100%',
-                      fontSize: 25,
-                      textAlign: 'end',
-                      marginLeft: '10px',
-                      color: '#353239',
-                    }}
-                  ></span>
-                }
-                id='icon-switch'
-              />
             </div>
           </div>
         </div>
